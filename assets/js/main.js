74e3138af4f01c48426b4a22677c0943cce11be6
Pradeep PL <pradeeppl005@gmail.com>
2021-05-25 14:35:52 +0530

    code coomit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+$("#banking .slideLink").click(function () {
+        $(".projectDet").removeClass("active");
+        $("#retail").addClass("active");
+});
+$("#retail .slideLink").click(function () {
+    $(".projectDet").removeClass("active");
+    $("#corporate").addClass("active");
+});
+$("#corporate .slideLink").click(function () {
+    $(".projectDet").removeClass("active");
+    $("#banking").addClass("active");
+});
