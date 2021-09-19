14a8540447fface9c067c8dbec55e5235a0c7dcc
hariz <harizanchal@gmail.com>
2021-09-19 12:15:10 +0530

    bb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 
 $("#banking .slideLink").click(function () {
-        $(".projectDet").removeClass("active");
-        $("#retail").addClass("active");
+    $(".projectDet").removeClass("active");
+    $("#retail").addClass("active");
 });
 $("#retail .slideLink").click(function () {
     $(".projectDet").removeClass("active");
     $("#corporate").addClass("active");
 });
 $("#corporate .slideLink").click(function () {
+    $(".projectDet").removeClass("active");
+    $("#healthcare").addClass("active");
+});
+$("#healthcare .slideLink").click(function () {
     $(".projectDet").removeClass("active");
     $("#banking").addClass("active");
 });
